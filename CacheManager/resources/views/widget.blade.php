f7fc2633fea41a54ac6c40a9276750b4092ce94a
Rémy <git@rrelmy.ch>
2017-07-21 22:20:46 +0200

    fix for statamic 2.6

--- CacheManager/resources/views/widget.blade.php
+++ CacheManager/resources/views/widget.blade.php
@@ -7,7 +7,7 @@
                 <i class="icon icon-dots-three-vertical"></i>
             </button>
             <ul class="dropdown-menu">
-                <li><a href="/{{ $cp_path }}/settings/cp"><span class="icon icon-browser" aria-hidden="true"></span> Widget Settings</a></li>
+                <li><a href="{{ route('settings.edit', 'cp') }}"><span class="icon icon-browser" aria-hidden="true"></span> Widget Settings</a></li>
                 <li><a href="{{ $github_page }}" target="_blank"><span class="icon icon-github" aria-hidden="true"></span> Github Page</a></li>
             </ul>
         </div>
@@ -19,12 +19,12 @@
             <tbody>
                 <tr>
                     <td>
-                        <a href="/{{ $cp_path }}/addons/cachemanager/clear-cache"><em class="icon icon-trash"></em> Clear Cache</a>
+                        <a href="{{ route('cachemanager.clear-cache') }}"><em class="icon icon-trash"></em> Clear Cache</a>
                     </td>
                 </tr>
                 <tr>
                     <td>
-                        <a href="/{{ $cp_path }}/addons/cachemanager/update-stache"><em class="icon icon-cw"></em> Update Stache</a>
+                        <a href="{{ route('cachemanager.update-stache') }}"><em class="icon icon-cw"></em> Update Stache</a>
                     </td>
                 </tr>
             </tbody>
